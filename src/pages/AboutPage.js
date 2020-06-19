40e0fb1b345135dcb9892ccd19be069aeecf7d86
Bryan Roof <broof716@gmail.com>
2020-06-19 11:25:14 -0700

    Initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <>
+  <h1>About me</h1>
+  <p>
+    Welcome to my blog! I am currently looking for a sofware
+    engineering position. I have a lot of experience in 
+    React.js, JavaScript, Node.js, and Ruby on Rails.
+  </p>
+  <p>
+    I have currently been working on some React projects and 
+    freelancing. Linkedin Learning is a great tool to use if 
+    you are wanting to become a better coder. 
+  </p>
+  </>
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+  <h1>Hello, welcome to my blog!</h1>
+  <p>
+    Welcome to my blog! I am currently looking for a sofware
+    engineering position. I have a lot of experience in 
+    React.js, JavaScript, Node.js, and Ruby on Rails.
+  </p>
+  <p>
+    I have currently been working on some React projects and 
+    freelancing. Linkedin Learning is a great tool to use if 
+    you are wanting to become a better coder. 
+  </p>
+  </>
+);
+
+export default HomePage;
